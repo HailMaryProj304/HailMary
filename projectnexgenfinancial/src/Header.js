b4b42627b4dd404f9903b386db3a41d2d3061686
Raymond <raymond.duong@edu.sait.ca>
2021-02-22 21:57:18 -0700

    Added CSS elements

--- projectnexgenfinancial/src/Header.js
+++ projectnexgenfinancial/src/Header.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import logo from './logo1.png';
-import { Link } from 'react-router-dom';
-import './Header.css';
+import React from "react";
+import logo from "./logo.png";
+import {Link} from "react-router-dom";
+import "./Header.css";
 
 function Header() {
-    return (
-        <div className="Header">
-            <img src={logo} width='350px'></img> 
-            <button><Link to="/about">About</Link></button>
-            <button><Link to="/login">Login</Link></button>
-            <button><Link to="/">Home</Link></button>
+  return (
+    <div className="Header">
+      <div className="Menu">
+        <a><Link to="/"><img src={logo} width="198px"></img></Link></a>
+        <div className="navigation-items">
+          <a className="navigation-item w-nav-link"><Link to="/about">About</Link></a>
+          <a className="navigation-item w-nav-link">Form</a>
+          <a className="navigation-item w-nav-link">FAQ</a>
+          <a className="navigation-item w-nav-link">Contact</a>
         </div>
-    )
+        <a><Link to="/login" className="button">Login</Link></a>
+      </div>
+    </div>
+  );
 }
 export default Header;
