f35ab1fe35e6d31421aee5b0c9572efcfe20d1ba
ColeKepford <cole.kepford@gmail.com>
2021-02-23 16:29:32 -0700

    login

--- projectnexgenfinancial/src/Login.js
+++ projectnexgenfinancial/src/Login.js
@@ -1,49 +1,48 @@
 
 import React, {useState} from 'react';
 import LoginPage from './components/LoginPage';
+import axios from 'axios';
 
 function Login() {
-  //this user is to test logiing in replace with mysql function
-    const testUser = {username: "test@test.com", password: "password"}
+
     //contains information of the user
     const [user, setUser] = useState({username: "", email: ""});
-    //contains information of the error message that is sent 
+    //contains information of the error message that is sent
     const [error, setError] = useState("");
 
     //function login pass in details
     const Validate = details => {
-      console.log(details);
+        console.log(details);
 
-      if(details.email == testUser.email && details.password == testUser.password) {
-        console.log("logged in");
-        setUser({username: details.username, email: details.email});
-      }
-
-      else {
-        console.log("Credentails do not match");
-        setError("Your credentials do not match!");
-      }
+        axios.get("http://localhost:8080/login/" + details.username + "&" + details.password)
+        .then((response) => {
+            setError("");
+            const testUser = response.data;
+            setUser(testUser);
+        }, (error) => {
+            setError("Your credentials are invalid");
+        });
     }
 
     //function logout
     const Logout = () => {
-      console.log("Logout");
-      setUser({username: "", email: ""});
+        console.log("Logout");
+        setUser({username: "", email: ""});
     }
 
     return (
-      <div className="App">
-        {/* if the email is null show this page */}
-          {(user.email != "") ? (
-            <div className="welcome">
-              <h2>Welcome, <span>{user.username}</span></h2>
-              {/* button calls logout function that sets fields to null */}
-              <button onClick={Logout}> Logout</button>
-            </div>
-    ) : (
-     
-      <LoginPage Login={Validate} error={error}/>
-    )}
-    </div>
+        <div className="App">
+            {/* if the email is null show this page */}
+            {(user.email != "") ? (
+                <div className="welcome">
+                    <h2>Welcome, <span>{user.username}</span></h2>
+                    {/* button calls logout function that sets fields to null */}
+                    <button onClick={Logout}> Logout</button>
+                </div>
+            ) : (
+
+                <LoginPage Login={Validate} error={error}/>
+            )}
+        </div>
     )}
 export default Login;
