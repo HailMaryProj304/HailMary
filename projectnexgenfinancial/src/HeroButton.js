ceb3185ad02a98851728e040a7eb4e6e3f04a894
Raymond <raymond.duong@edu.sait.ca>
2021-03-04 01:41:31 -0700

    Fixed hero page

--- projectnexgenfinancial/src/HeroButton.js
+++ projectnexgenfinancial/src/HeroButton.js
@@ -7,12 +7,13 @@ function HeroButton() {
     <div className="hero-image">
       <div className="HeroButton">
         <div className="intro-text">
-          <h1 style={{ fontSize: "64px" }}>
-            NexGen is here to help.
-          </h1>
-          <h3 style={{ fontSize: "20px" }}>Let us help you find the perfect plan.</h3>
+          <h1 style={{ fontSize: "64px" }}>NexGen is here to help.</h1>
+          <h3 style={{ fontSize: "20px" }}>
+            Let us help you find the perfect plan.
+          </h3>
         </div>
-        <Link to="/about" className="button cc-jumbo-button cc-jumbo-white">{/* the link was "/start" but I (Raymond) changed it for the use case demo */}
+        <Link to="/about" className="button cc-jumbo-button cc-jumbo-white">
+          {/* the link was "/start" but I (Raymond) changed it for the use case demo */}
           LEARN MORE
         </Link>
       </div>
